9afc9432d12d6686a8d9b75260ee323f4c694909
ullasmo <ullasa.me18@bmsce.ac.in>
2024-02-16 17:27:34 +0530

    React and tailwind installed

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function App() {
+  return <h1 className="text-red-500 text-center font-bold ">App</h1>;
+}
+
+export default App;
